4111743ee9fb7867e1f350a278ee93117bc18457
Erik Veenstra <zerikveenstraz@gmail.com>
2024-07-27 22:00:55 +0200

    Add project files.

---
+++ DriveHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ExternalDriveBackupCreator;
+
+internal class DriveHelper
+{
+    public static List<DriveInfo> GetUsableDrives()
+    {
+        return DriveInfo.GetDrives()
+            .Where(d => d.IsReady && d.DriveType == DriveType.Removable)
+            .ToList();
+    }
+}
